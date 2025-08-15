07cc7b907d5a2be1eb8c35ed91f5de4933d062d3
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-08-14 22:05:50 -0300

    criação projeto

---
+++ src/main/java/com/luccasaps/projetopds/ProjetoPdsApplication.java
@@ -0,0 +1,13 @@
+package com.luccasaps.projetopds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoPdsApplication.class, args);
+    }
+
+}
