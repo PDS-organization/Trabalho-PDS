ff576eb72906f7a6b192b642b4879fa412092286
MuriloMelo <muriloxmelo@gmail.com>
2025-08-16 11:57:12 -0300

    feat/frontend-init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+};
