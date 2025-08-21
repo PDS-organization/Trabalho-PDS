65c6e067afaf9a576eac162b6cf123e7e8ffa460
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-08-20 23:07:04 -0300

    retornando usuario criado e JSON correto de criação de usuario

--- ProjetoPDS/src/main/java/com/luccasaps/projetopds/controller/UserController.java
+++ ProjetoPDS/src/main/java/com/luccasaps/projetopds/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.luccasaps.projetopds.controller;
 
 import com.luccasaps.projetopds.controller.dto.UserDTO;
-import com.luccasaps.projetopds.controller.mappers.UserMapper;
+import com.luccasaps.projetopds.controller.dto.UserResponseDTO;
+import com.luccasaps.projetopds.controller.mappers.UserResponseMapper;
 import com.luccasaps.projetopds.model.User;
-import com.luccasaps.projetopds.service.ModalidadeService;
 import com.luccasaps.projetopds.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,13 +18,15 @@ import java.net.URI;
 public class UserController implements GenericController {
 
     private final UserService userService;
+    private final UserResponseMapper userResponseMapper;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> save(@RequestBody @Valid UserDTO userDTO){
-        User newUser = userService.save(userDTO);
+    public ResponseEntity<UserResponseDTO> save(@RequestBody @Valid UserDTO userDTO){
+        User user = userService.save(userDTO);
 
-        URI location = gerarHeaderLocation(newUser.getId());
-        return ResponseEntity.created(location).build();
+        UserResponseDTO userResponse = userResponseMapper.toResponseDTO(user);
+
+        URI location = gerarHeaderLocation(userResponse.id());
+        return ResponseEntity.created(location).body(userResponse);
     }
 }
--- ProjetoPDS/src/main/java/com/luccasaps/projetopds/controller/mappers/ModalidadeMapper.java
+++ ProjetoPDS/src/main/java/com/luccasaps/projetopds/controller/mappers/ModalidadeMapper.java
@@ -1,6 +1,5 @@
 package com.luccasaps.projetopds.controller.mappers;
 
-import com.luccasaps.projetopds.controller.dto.ApiResponseDTO;
 import com.luccasaps.projetopds.controller.dto.ModalidadeDTO;
 import com.luccasaps.projetopds.model.Modalidade;
 import org.mapstruct.Mapper;
